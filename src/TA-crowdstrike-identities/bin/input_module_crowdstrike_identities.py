144ce978b7248a00684be18c389eaa3283a51ec7
Zachary Christensen <zchristensen@splunk.com>
2023-05-08 22:04:03 -0600

    updated data collection
    Signed-off-by: Zachary Christensen <zchristensen@splunk.com>

--- src/TA-crowdstrike-identities/bin/input_module_crowdstrike_identities.py
+++ src/TA-crowdstrike-identities/bin/input_module_crowdstrike_identities.py
@@ -60,11 +60,11 @@ def collect_events(helper, ew):
 
     event_type = 'api_call'
     event_log = zts_logger(
-        msg = 'Sending request',
-        action = 'started',
-        event_type = event_type,
+        msg='Sending request',
+        action='started',
+        event_type=event_type,
         stanza=stanza,
-        hostname = hostname,
+        hostname=hostname,
         base_url=cloud_env,
         user_agent=f'{user_agent}/{APP_VERSION}'
     )
@@ -78,31 +78,79 @@ def collect_events(helper, ew):
                                 )
 
     idp_query = """
-    query ($after: Cursor) {
-    entities(types: [USER], archived: false, learned: false, first: 5, after: $after) {
-        nodes {
-        primaryDisplayName
-        secondaryDisplayName
-        accounts {
-            ... on ActiveDirectoryAccountDescriptor {
-            domain
+    query {
+        entities(
+        sortKey: PRIMARY_DISPLAY_NAME
+        sortOrder: DESCENDING
+        first: 1000) {
+            nodes {
+                primaryDisplayName
+                secondaryDisplayName
+                riskScore
+                riskScoreSeverity
+                type
+                roles { type }
+                isProgrammatic: hasRole(type: ProgrammaticUserAccountRole) ... on UserEntity {
+                    emailAddresses
+                }
+                watched
+                accounts { ... on ActiveDirectoryAccountDescriptor {
+                    domain
+                    samAccountName
+                    dn
+                    department
+                    ou
+                    }
+                }
+            }
+            pageInfo {
+                hasNextPage
+                endCursor
             }
         }
-        }
-        pageInfo {
-        hasNextPage
-        endCursor
-        }
-    }
     }
     """
 
-    variables = {
-        "string": "string, int, float"
-    }
+    response = falcon.api_preempt_proxy_post_graphql(query=idp_query, variables=None)
+    status_code = response['status_code']
 
-    response = falcon.api_preempt_proxy_post_graphql(query=idp_query, variables=variables)
-    
-    splunk_event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(), sourcetype=helper.get_sourcetype(), data=json.dumps(response), host=hostname)
-    
-    ew.write_event(splunk_event)
+    if status_code != 200:
+        error_msg = response["body"]["errors"]
+        event_log = zts_logger(
+            msg='Request failed',
+            action='failure',
+            event_type=event_type,
+            stanza=stanza,
+            hostname=hostname,
+            http_status_code=status_code,
+            error_message=json.dumps(error_msg),
+            base_url=cloud_env,
+            user_agent=f'{user_agent}/{APP_VERSION}'
+        )
+        helper.log_error(event_log)
+        exit(status_code)
+
+    event_log = zts_logger(
+        msg='Request succeeded',
+        action='success',
+        event_type=event_type,
+        stanza=stanza,
+        hostname=hostname,
+        base_url=cloud_env,
+        user_agent=f'{user_agent}/{APP_VERSION}'
+    )
+    helper.log_info(event_log)
+
+    data = response["body"]["data"]["entities"]["nodes"]
+    page_info = response["body"]["data"]["entities"]["pageInfo"]
+
+    for account in data:
+        splunk_event = helper.new_event(source=helper.get_input_type(), index=helper.get_output_index(
+        ), sourcetype=helper.get_sourcetype(), data=json.dumps(account), host=hostname)
+        ew.write_event(splunk_event)
+
+    # TODO: Finish Pagination
+    # next_page_exists = page_info['hasNextPage']
+    # while next_page_exists == "true":
+    #     continue
+
